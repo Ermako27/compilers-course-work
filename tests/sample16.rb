d7f1dd2a68e976e9acf6a63ae4138c294baf350a
Ermako27 <ermakov.1828@gmail.com>
2021-09-10 00:47:43 +0300

    добавил пример

--- tests/sample16.rb
+++ tests/sample16.rb
@@ -10,6 +10,15 @@ def foo1(arg1)
             end
         end
     end
+    myVar1 = 10 * 2
+
+    if myVar1 > 20
+        newVr1 = 20
+    elsif myVar1 == 20
+        newVar1 = 30
+    else
+        newVar1 = 40
+    end
 end
 
 def foo2(arg1,arg2)
